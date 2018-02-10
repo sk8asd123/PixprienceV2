f4d3b460a156025c78cfe4ab6420b85a7d52c915
Paige Lo <sk8asd123@gmail.com>
2018-02-10 15:15:01 -0600

    image loading works

--- client/src/components/Upload/Pixuploader.js
+++ client/src/components/Upload/Pixuploader.js
@@ -15,7 +15,7 @@
 
 // //   handleSubmit(event) {
 // //     event.preventDefault();
-    
+
 
 // //     console.log("upload clicked");
 
@@ -25,7 +25,7 @@
 
 // //     this.props.setImagePreview(imageFile);
 // //   }
-  
+
 
 // //   componentDidMount(){
 // //     let that = this;
@@ -35,7 +35,7 @@
 // //       console.log(imageFile);
 
 // //       // console.log(file.name);
-      
+
 // //        that.props.setImagePreview(imageFile )
 // //       // alert(this.file)
 // //     }
@@ -49,7 +49,7 @@
 // //           <input
 // //             type="file"
 // //             ref= "imageUpload"
-// //             style={displayNone} 
+// //             style={displayNone}
 // //             id = "file-input"
 // //           />
 // //           {/* <input type="submit" className="button" /> */}
@@ -142,10 +142,10 @@
 
 //     return (
 //       <div className="previewComponent">
-        
+
 //         <Row>
 //           <Col s={2}></Col>
-//           <Col s={8} className="center-align"> 
+//           <Col s={8} className="center-align">
 //             <div className="imgPreview">
 //               {$imagePreview}
 //             </div>
@@ -155,27 +155,27 @@
 //         <Row>
 //         <FileBase64
 //           multiple={ true }
-//           onDone={ this.handleFileUpload.bind(this)} 
+//           onDone={ this.handleFileUpload.bind(this)}
 //           onChange={(e)=>this._handleSubmit(e)}
 //           style={hideDisplay} />
-          
+
 //             <form onSubmit={(e)=>this._handleSubmit(e)}>
 //               <Col s={12} className="center-align">
 //                 <label>
 //                   <span className="attach-doc hoverColor">Upload an Image &nbsp;<i className="fa fa-upload" aria-hidden="true"></i></span>
-//                   <input className="fileInput" 
-//                     type="file" 
-//                     onChange={(e)=>this._handleImageChange(e)} 
+//                   <input className="fileInput"
+//                     type="file"
+//                     onChange={(e)=>this._handleImageChange(e)}
 //                     style={hideDisplay}/>
 //                 </label>
 //               </Col>
 //               <Col s={12}  className="center-align">
-//                 <button className="submitButton" 
-//                   type="submit" 
+//                 <button className="submitButton"
+//                   type="submit"
 //                   onClick={this.handleSubmit}>Submit</button>
 //               </Col>
 //             </form>
-          
+
 //         </Row>
 //       </div>
 //     )
@@ -197,58 +197,60 @@ import ImageUploader from 'react-images-upload';
 
 
 class Pixupload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      //photos: [
-        img: [],
-        location: " ",
-        notes: "",
-        // file = this.fileInput.files[0],
-        // fileName = file.name,
-        // loc = this.textInput;
-        // notes = this.textInput;
-      //]
-    };
+    constructor(props) {
+        super(props);
+        this.state={
+            //photos: [
+            img: [],
+            notes: "",
+            title: "",
+            // file = this.fileInput.files[0],
+            // fileName = file.name,
+            // loc = this.textInput;
+            // notes = this.textInput;
+            //]
+        };
 
-    //CALLBACK USED WHEN CHANGING PHOTO FILE TO BASE 64
-    // getFiles(files){
-    //   this.setState({ files: files })
-    // }
+        //CALLBACK USED WHEN CHANGING PHOTO FILE TO BASE 64
+        // getFiles(files){
+        //   this.setState({ files: files })
+        // }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChangeNotes = this.handleInputChangeNotes.bind(this);
-    this.handleFileUpload = this.handleFileUpload.bind(this);
-  }
-  //FUNCTION FOR WHAT HAPPENS WHEN SUBMIT BUTTON IS CLICKED AKA COLLECTING AND SENDING FILE
-  handleSubmit(event) {
-    event.preventDefault();
-    // console.log('THIS IS FILE INPUT ____----', this.fileInput);
-    // console.log(this.fileInput.files[0]);
-    // console.log(this.textInput);
-    // console.log(this.fileInput.files[0].name);
-    // var pixObject = {
-    //       //var user_id: sessionStorage.getItem("userID"),
-    //       file: this.fileInput.files[0],
-    //       fileName: this.fileInput.files[0].name,
-    //       loc: this.textInput,
-    //       notes: this.textInput
-    //     };
-  //SENDING THE PIC DATA TO THE BACKEND
-  // console.log('we are about to save this thing ----', pixObject);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChangeNotes = this.handleInputChangeNotes.bind(this);
+        this.handleInputChangeTitle = this.handleInputChangeTitle.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+    }
+    //FUNCTION FOR WHAT HAPPENS WHEN SUBMIT BUTTON IS CLICKED AKA COLLECTING AND SENDING FILE
+    handleSubmit(event) {
+        event.preventDefault();
+        // console.log('THIS IS FILE INPUT ____----', this.fileInput);
+        // console.log(this.fileInput.files[0]);
+        // console.log(this.textInput);
+        // console.log(this.fileInput.files[0].name);
+        // var pixObject = {
+        //       //var user_id: sessionStorage.getItem("userID"),
+        //       file: this.fileInput.files[0],
+        //       fileName: this.fileInput.files[0].name,
+        //       loc: this.textInput,
+        //       notes: this.textInput
+        //     };
+        //SENDING THE PIC DATA TO THE BACKEND
+        // console.log('we are about to save this thing ----', pixObject);
 
-        var tom = {
-          base64: this.state.img[0].base64,
-          notes: this.state.notes
+        var data = {
+            base64: this.state.img[0].base64,
+            title: this.state.title,
+            notes: this.state.notes
         }
 
-        axios.post('http://localhost:8080/api/upload', tom)
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+        axios.post('/api/upload', data)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         //
         // fetch("http://localhost:8080/api/upload", {
         //   method: 'POST',
@@ -268,75 +270,79 @@ class Pixupload extends React.Component {
         //     console.log("Upload Failed");
         //   }
         // });
-  }
+    }
 
-  handleInputChangeNotes(e) {
-    console.log('handle inpout chance notes@@');
-    this.setState({notes: e.target.value})
-  }
-  handleFileUpload(picture) {
-    this.setState({
-        img: this.state.img.concat(picture),
-    });
-  }
+    handleInputChangeNotes(e) {
+        console.log('handle inpout chance notes@@');
+        this.setState({notes: e.target.value})
+    }
+    handleInputChangeTitle(e) {
+        console.log('handle inpout chance notes@@');
+        this.setState({title: e.target.value})
+    }
+    handleFileUpload(picture) {
+        this.setState({
+            img: this.state.img.concat(picture),
+        });
+    }
 
 //CREATION OF THE FORM UI
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-      {/*<form
-        onSubmit={this.handleSubmit}
-        > */}
-        {/*<label>
-          CHOOSE A PIC:
-          <input
-            name="img"
-            type="file"
-            ref={input => {
-              this.fileInput = input;
-            ;
-            }}
-            onChange={this.handleFileUpload}
-          />
-        </label>*/}
-        <FileBase64
-          multiple={ true }
-          onDone={ this.handleFileUpload.bind(this)}
-        />
-        <br />
-        <label>
-         Location:
-         <input
-           name="location"
-           type="text"
-           ref={input => {
-             this.textInput = input;
-             console.log(input);
-             ;
-           }}
-           //value={this.state.notes}
-           onChange={this.handleInputChange} />
-       </label>
-       <br />
-       <label>
-         Notes:
-         <input
-           name="notes"
-           type="text"
-           ref={input => {
-             this.textInput = input;
-             ;
-           }}
-           //value={this.state.notes}
-           onChange={this.handleInputChangeNotes} />
-       </label>
-        <button onClick={this.handleSubmit}>
-          SUBMIT UR PIC :)
-        </button>
-        </div>
-    );
-  }
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                {/*<form
+                 onSubmit={this.handleSubmit}
+                 > */}
+                {/*<label>
+                 CHOOSE A PIC:
+                 <input
+                 name="img"
+                 type="file"
+                 ref={input => {
+                 this.fileInput = input;
+                 ;
+                 }}
+                 onChange={this.handleFileUpload}
+                 />
+                 </label>*/}
+                <FileBase64
+                    multiple={ true }
+                    onDone={ this.handleFileUpload.bind(this)}
+                />
+                <br />
+                <label>
+                    Title:
+                    <input
+                        name="title"
+                        type="text"
+                        ref={input => {
+                            this.textInput = input;
+                            console.log(input);
+                            ;
+                        }}
+                        //value={this.state.notes}
+                        onChange={this.handleInputChangeTitle} />
+                </label>
+                <br />
+                <label>
+                    Notes:
+                    <input
+                        name="notes"
+                        type="text"
+                        ref={input => {
+                            this.textInput = input;
+                            ;
+                        }}
+                        //value={this.state.notes}
+                        onChange={this.handleInputChangeNotes} />
+                </label>
+                <button onClick={this.handleSubmit}>
+                    SUBMIT UR PIC :)
+                </button>
+            </div>
+        );
+    }
 }
 
 export default Pixupload
--- server.js
+++ server.js
@@ -5,7 +5,9 @@
 /////////////////////////////////////////////// /* Imports */ //////////////////////////////////////////////////////////
 const express = require('express'); // Server
 const bodyParser = require ('body-parser'); // JSON Middleware
-//const logger = require('morgan'); // REST Logger
+
+// const logger = require('morgan'); // REST Logger
+
 
 
 /////////////////////////////////////////////// /* Initialize Express */ //////////////////////////////////////////////////////////
@@ -22,51 +24,70 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 const mongoose = require('mongoose'); // MongoDB ORM
 mongoose.Promise = global.Promise; // Set up promises with mongoose
 const mongooseConnection = mongoose.connection;
-const db = require("./models"); // Sequelize Models
+const db = require("./models");
+const routes = require("./routes");
 
 
 mongoose.connect( // Connect to the Mongo DB
-  process.env.MONGODB_URI ||"mongodb://heroku_0g1bl4gg:heroku_0g1bl4gg@ds123718.mlab.com:23718/heroku_0g1bl4gg",
-  {
-    useMongoClient: true
-  }
+    process.env.MONGODB_URI ||"mongodb://test:test@ds123718.mlab.com:23718/heroku_0g1bl4gg",
+    {
+        useMongoClient: true
+    }
 );
 
 mongooseConnection.on("error", console.error.bind(console, "connection error:"));
 
 mongooseConnection.once("open", function() {
-  console.log("Sucessfully Connected to Mongo DB !"); // If Connection is successful, Console.log(Message)
+    console.log("Sucessfully Connected to Mongo DB !"); // If Connection is successful, Console.log(Message)
 });
 
 
 /////////////////////////////////////////////// /* Routes */ //////////////////////////////////////////////////////////
-// const routes = require("./routes");
-// app.use(routes); // Add routes, both API and View
+// Authenication Routes //
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
 
 /////////////////////////////////////////////// /* Passport */ //////////////////////////////////////////////////////////
 
 // Passport Validation //
-const passport = require('passport');
+// const passport = require('passport');
 
 // load passport strategies //
-const localSignupStrategy = require('./passport/localSignup');
-const localLoginStrategy = require('./passport/localLogin');
-passport.use('local-signup', localSignupStrategy);
-passport.use('local-login', localLoginStrategy);
+// const localSignupStrategy = require('./passport/localSignup');
+// const localLoginStrategy = require('./passport/localLogin');
+// passport.use('local-signup', localSignupStrategy);
+// passport.use('local-login', localLoginStrategy);
 
+/////////////////////////////////////////////// /* Cross Origin Settings */ //////////////////////////////////////////////////////////
 var cors = require("cors");
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 app.use(cors());
 
+// Image Upload Route
 app.post("/api/upload", function(req, res) {
-  console.log("Submit Images Path hit");
-  console.log(req.body);
-  res.json({name: 'tom'})
+    console.log("Submit Images Path hit");
+    console.log(req.body)
+    console.log(req.body.title);
+    console.log(req.body.notes);
+
+    // res.json({name: 'tom'})
+    db.Image.create({
+        image: req.body.base64,
+        title: req.body.title,
+        notes: req.body.notes
+    })
+        .then(function(dbImage){
+            console.log(dbImage);
+        })
+        .catch(function(err){
+            console.log(err.message);
+        })
 });
+
 
 
 
@@ -80,3 +101,12 @@ app.listen(PORT, (error) => {
         throw error;
     }
 });
+
+
+// db.Image.create({ title: 'test2000'})
+// .then(function(dbImage){
+//     console.log(dbImage);
+// })
+// .catch(function(err){
+//     console.log(err.message);
+// })
