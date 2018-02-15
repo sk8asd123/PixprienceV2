f0b480df7ac052b4072b7632565d3cf99399dc2d
Babak Shahian Jahromi <BabakShah@users.noreply.github.com>
2018-02-14 20:22:13 -0600

    Added query based on emails as id.

--- server.js
+++ server.js
@@ -3,29 +3,28 @@
 "use strict";
 
 /////////////////////////////////////////////// /* Imports */ ////////////////////////////////////////////////////////
+
 const express = require('express'); // Server
 const bodyParser = require('body-parser'); // JSON Middleware
 const passport = require('passport');
 const config = require('./config');
 
-// const logger = require('morgan');  REST Logger
-
 /////////////////////////////////////////////// /* Initialize Express */ ////////////////////////////////////////////////////////
+
 let app = express();
 let PORT = process.env.PORT || 8080;
 
 /////////////////////////////////////////////// /* Express Middleware */ ////////////////////////////////////////////////////////
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // Allows For JSON Interactions Between Client & Server
 app.use(express.static("client/build")); // Serve Static React Pages
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
-
-
 require('./models').connect(config.dbUri); // connect to the database and load models
 
-
 /////////////////////////////////////////////// /* Passport Authentication */ //////////////////////////////////////////////////////////
+
 // load passport strategies
 const localSignupStrategy = require('./passport/local-signup');
 const localLoginStrategy = require('./passport/local-login');
@@ -35,7 +34,6 @@ passport.use('local-login', localLoginStrategy);
 // pass the authenticaion checker middleware //
 const authCheckMiddleware = require('./middleware/auth-check');
 
-
 /////////////////////////////////////////////// /* Routes */ ////////////////////////////////////////////////////////
 
 const authRoutes = require('./routes/auth');
@@ -44,8 +42,8 @@ app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 app.use('/api', authCheckMiddleware);
 
-
 /////////////////////////////////////////////// /* Cross Origin Settings */ ////////////////////////////////////////////////////////
+
 var cors = require("cors");
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -55,6 +53,7 @@ app.use(function(req, res, next) {
 app.use(cors());
 
 /////////////////////////////////////////////// /* Hard Coded Routes */ //////////////////////////////////////////////////////////
+
 const db = require("./models"); // Sequelize Models
 // images Upload Route
 app.post("/test/upload", function(req, res) {
@@ -63,7 +62,6 @@ app.post("/test/upload", function(req, res) {
   console.log(req.body.title);
   console.log(req.body.notes);
 
-  // res.json({name: 'tom'})
   db.Image.create({image: req.body.base64, title: req.body.title, notes: req.body.notes}).then(function(dbImage) {
     console.log(dbImage);
   }).catch(function(err) {
@@ -73,8 +71,10 @@ app.post("/test/upload", function(req, res) {
 
 app.get("/test/images", function(req, res) {
   console.log("images path hit.")
-  db.Image.find({}, function(err, found) {
-
+  console.log(req.query.email);
+  db.Image.find({ 'email': req.query.email }, function(err, found) {
+    console.log(found)
+    // console.log("images of user with "+ email +" found.")
     // Log any errors if the server encounters one
     if (err) {
       console.log(err);
@@ -86,16 +86,8 @@ app.get("/test/images", function(req, res) {
   });
 });
 
-// db.images.create({ title: 'test2000'})
-// .then(function(dbimages){
-//     console.log(dbimages);
-// })
-// .catch(function(err){
-//     console.log(err.message);
-// })
-
-
 /////////////////////////////////////////////// /* Start Server */ ////////////////////////////////////////////////////////
+
 app.listen(PORT, (error) => {
   if (!error) {
     console.log("listening on port", PORT);
